b2097e18161e22df337bb99dca4403ef08d314fa
Bria <bnmears@gmail.com>
2019-01-13 03:36:35 -0500

    templates & color schemes updates

---
+++ page-templates/template-orange.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Orange Color Scheme */
+/**
+ * The template for displaying pages with the orange color scheme.
+ *
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+use Timber\Timber;
+
+$context        = Timber::get_context();
+$post           = new P4_Post();
+$page_meta_data = get_post_meta( $post->ID );
+
+$context['post']                = $post;
+$context['custom_body_classes'] = 'template-orange';
+
+Timber::render( array( 'page.twig' ), $context );
---
+++ page-templates/template-yellow.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Yellow Color Scheme */
+/**
+ * The template for displaying pages with the yellow color scheme.
+ *
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+use Timber\Timber;
+
+$context        = Timber::get_context();
+$post           = new P4_Post();
+$page_meta_data = get_post_meta( $post->ID );
+
+$context['post']                = $post;
+$context['custom_body_classes'] = 'template-yellow';
+
+Timber::render( array( 'page.twig' ), $context );
