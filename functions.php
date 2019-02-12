bf44bab7e1fa1424d0561c6e7ac34a4cb9cdb41b
Bria <bnmears@gmail.com>
2019-02-12 16:16:08 -0500

    links leading to another url open in a new tab

--- functions.php
+++ functions.php
@@ -10,3 +10,9 @@ function enqueue_child_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
+
+function enqueue_child_js() {
+	wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/_src/js/main.js', array(), 1, true );
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue_child_js', 99);
