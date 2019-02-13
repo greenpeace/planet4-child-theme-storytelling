9c58c46a5c2fef81d1b36efa3709a388efca403d
Bria <bnmears@gmail.com>
2019-02-13 14:14:36 -0500

    update CSS version

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
 
 function enqueue_child_styles() {
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'main-style', get_stylesheet_directory_uri() . '/_src/css/crate.css', array(), 6.2, 'all' );
+	wp_enqueue_style( 'main-style', get_stylesheet_directory_uri() . '/_src/css/crate.css', array(), 6.3, 'all' );
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
