5075e07ee1966a1a788febecf7bb42b68e4617b3
Bria <bnmears@gmail.com>
2019-03-06 16:33:59 -0500

    QA refinements

--- functions.php
+++ functions.php
@@ -16,3 +16,29 @@ function enqueue_child_js() {
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_child_js', 99);
+
+
+/**
+ * Add Storytelling Logo to Login Page
+ */
+function my_login_logo() { ?>
+    <style type="text/css">
+    	body.login{
+    		background: #3ea19e;
+    	}
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logos/whale-logo.png);
+			background-size: contain;
+		    background-repeat: no-repeat;
+		    width: 100%;
+		    height: 47px;
+        }
+        #login #backtoblog a, #login #nav a{
+        	color: #fff;
+        }
+        #login #backtoblog a:hover, #login #nav a:hover{
+        	color: #f7e8d7;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
